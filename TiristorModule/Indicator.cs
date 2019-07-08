797cabf55db88e39d16820a31cdf5b2995b7dfae
Grishchenko Timofei <timoxa810@gmail.com>
2019-07-08 11:59:42 +0300

    creating model

---
+++ TiristorModule/Indicator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TiristorModule
+{
+    public class Indicator: INotifyPropertyChanged
+    {
+        private double amperageA1;
+        private double amperageB1;
+        private double amperageC1;
+        private double amperageA2;
+        private double amperageB2;
+        private double amperageC2;
+        private double voltageA1;
+        private double voltageA2;
+        private double voltageA3;
+        private int tiristorTemperature;
+        private string tiristorStatus;
+
+        public double AmperageA1
+        {
+            get { return amperageA1; }
+            set
+            {
+                amperageA1 = value;
+                OnPropertyChanged("AmperageA1");
+            }
+        }
+
+        public double AmperageB1
+        {
+            get { return amperageB1; }
+            set
+            {
+                amperageA1 = value;
+                OnPropertyChanged("AmperageB1");
+            }
+        }
+
+        public double AmperageC1
+        {
+            get { return amperageC1; }
+            set
+            {
+                amperageA1 = value;
+                OnPropertyChanged("AmperageC1");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TiristorModule/TiristorModuleViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace TiristorModule
+{
+    class TiristorModuleViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Indicator selectedIndicator;
+
+        public ObservableCollection<Indicator> Indicators { get; set; }
+
+        public Indicator SelectedIndicator
+        {
+            get { return selectedIndicator; }
+            set
+            {
+                selectedIndicator = value;
+                OnPropertyChanged("SelectedIndicator");
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
