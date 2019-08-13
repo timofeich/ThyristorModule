4e8166bb8882ed126eeab69b0d2ed8f5feca9d06
Grishchenko Timofei <timoxa810@gmail.com>
2019-08-13 12:10:10 +0300

    add KZAmperage Validation

--- TiristorModule/Validation/KZApmerageRule.cs
+++ TiristorModule/Validation/KZApmerageRule.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Controls;
 
 namespace TiristorModule.Validation
 {
-    class KZApmerageRule
+     public class KZApmerageRule : ValidationRule
     {
+        public int Min { get; set; } = 0;
+        public int Max { get; set; } = int.MaxValue;
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo ci)
+        {
+            int requestInterval = 0;
+
+            try
+            {
+                requestInterval = int.Parse((string)value);
+            }
+
+            catch
+            {
+                return new ValidationResult(false, "Недопустимые символы.");
+            }
+
+            if ((requestInterval < Min) || (requestInterval > Max))
+            {
+                return new ValidationResult(false,
+                  "Значение токов KZ не входит в диапазон " + Min + " до " + Max + ".");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
     }
 }
--- TiristorModule/Validation/KZTimeRule.cs
+++ TiristorModule/Validation/KZTimeRule.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Controls;
 
 namespace TiristorModule.Validation
 {
-    class KZTimeRule
+    public class KZTimeRule:ValidationRule
     {
+        public int Min { get; set; } = 0;
+        public int Max { get; set; } = 100;
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo ci)
+        {
+            int requestInterval = 0;
+
+            try
+            {
+                requestInterval = int.Parse((string)value);
+            }
+
+            catch
+            {
+                return new ValidationResult(false, "Недопустимые символы.");
+            }
+
+            if ((requestInterval < Min) || (requestInterval > Max))
+            {
+                return new ValidationResult(false,
+                  "Значение мощности не входит в диапазон " + Min + " до " + Max + ".");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
     }
 }
