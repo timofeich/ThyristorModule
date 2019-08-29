a52bda037ac62233651a69a17c4f4c0fb14f8871
Grishchenko Timofei <timoxa810@gmail.com>
2019-08-29 10:15:42 +0300

    output correct data in testThyristorWindow

--- TiristorModule/Model/TestThyristorModel.cs
+++ TiristorModule/Model/TestThyristorModel.cs
@@ -1,27 +1,16 @@
-﻿namespace TiristorModule.Model
+﻿using System.ComponentModel;
+
+namespace TiristorModule.Model
 {
     public class TestThyristorModel
     {
-        private string FazaName { get; set; }
-        private ushort ApBn { get; set; }
-        private ushort BpAn { get; set; }
-        private ushort CpAn { get; set; }
-        private ushort ApCn { get; set; }
-        private ushort BpCn { get; set; }
-        private ushort CpBn { get; set; }
-        private ushort OpredelenieFazz { get; set; }
-
-        public TestThyristorModel(string fazaName, ushort apBn, ushort bpAn, ushort cpAn, ushort apCn, 
-            ushort bpCn, ushort cpBn, ushort opredelenieFazz)
-        {
-            FazaName = fazaName;
-            ApBn = apBn;
-            BpAn = bpAn;
-            CpAn = cpAn;
-            ApCn = apCn;
-            BpCn = bpCn;
-            CpBn = cpBn;
-            OpredelenieFazz = opredelenieFazz;
-        }
+        public string FazaName { get; set; }
+        public ushort ApBn { get; set; }
+        public ushort BpAn { get; set; }
+        public ushort CpAn { get; set; }
+        public ushort ApCn { get; set; }
+        public ushort BpCn { get; set; }
+        public ushort CpBn { get; set; }
+        public ushort OpredelenieFazz { get; set; }
     }
 }
